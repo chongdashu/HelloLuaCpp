eaaabce0018442cc59570538847f2b9448129504
Chong-U Lim <me@chongdashu.com>
2017-10-14 13:26:37 +0100

    Lesson 3: Calling Lua from C++

--- HelloLuaCpp/HelloLuaCpp.cpp
+++ HelloLuaCpp/HelloLuaCpp.cpp
@@ -38,6 +38,31 @@ int luaadd(int x, int y)
 	return sum;
 }
 
+// Lua calls this
+static int average(lua_State *L)
+{
+	/* get number of arguments */
+	int n = lua_gettop(L);
+	double sum = 0;
+	int i;
+
+	/* loop through each argument */
+	for (i = 1; i <= n; i++)
+	{
+		/* total the arguments */
+		sum += lua_tonumber(L, i);
+	}
+
+	/* push the average */
+	lua_pushnumber(L, sum / n);
+
+	/* push the sum */
+	lua_pushnumber(L, sum);
+
+	/* return the number of results */
+	return 2;
+}
+
 
 int main ( int argc, char *argv[] )
 {
@@ -78,6 +103,21 @@ int main ( int argc, char *argv[] )
 	lua_close(L);
 
 	// Lesson 3: Calling C++ from Lua
+
+	/* initialize Lua */
+	L = lua_open();
+
+	/* load Lua base libraries */
+	luaL_openlibs(L);
+
+	/* register our function */
+	lua_register(L, "average", average);
+
+	/* run the script */
+	luaL_dofile(L, "avg.lua");
+
+	/* cleanup Lua */
+	lua_close(L);
 
 	/* pause */
 	printf( "Press enter to exit..." );
